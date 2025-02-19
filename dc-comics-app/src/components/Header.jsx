33fc4d94b6013814e478cc00b3904b290961d4c4
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-19 17:26:07 +0100

    header con icona +link

--- dc-comics-app/src/components/Header.jsx
+++ dc-comics-app/src/components/Header.jsx
@@ -2,7 +2,25 @@ function Header(){
     return(
         <>
         <header>
-            header
+            <div className="container">
+                <figure>
+                    <img src="src/assets/img/dc-logo.png" alt="" />
+                </figure>
+                <nav>
+                    <ul>
+                        <li ><a href="#">link</a></li>
+                        <li className="active"><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
         </>
 
