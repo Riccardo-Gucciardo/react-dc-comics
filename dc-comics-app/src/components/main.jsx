edecb5a5a155febe9eb4d28a3efa0b2817117a2f
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-19 19:15:52 +0100

    items

--- dc-comics-app/src/components/main.jsx
+++ dc-comics-app/src/components/main.jsx
@@ -2,7 +2,37 @@ function Main(){
     return(
         <>
         <main>
-            main
+            <section id="fumetti">
+                <div className="container">
+                    <h2>--Content gose here--</h2>
+                </div>
+            </section>
+            <section id="icons">
+                <div className="container">
+                    <ul>
+                        <li>
+                            <figure><img src="src/assets/img/buy-comics-digital-comics.png" alt="icon" /></figure>
+                            <span>Nome icon</span>
+                        </li>
+                        <li>
+                            <figure><img src="src/assets/img/buy-comics-merchandise.png" alt="icon" /></figure>
+                            <span>Nome icon</span>
+                        </li>
+                        <li>
+                            <figure><img src="src/assets/img/buy-comics-shop-locator.png" alt="icon" /></figure>
+                            <span>Nome icon</span>
+                        </li>
+                        <li>
+                            <figure><img src="src/assets/img/buy-comics-subscriptions.png" alt="icon" /></figure>
+                            <span>Nome icon</span>
+                        </li>
+                        <li>
+                            <figure><img src="src/assets/img/buy-dc-power-visa.svg" alt="icon" /></figure>
+                            <span>Nome icon</span>
+                        </li>
+                    </ul>
+                </div>
+            </section>
         </main>
         </>
 
